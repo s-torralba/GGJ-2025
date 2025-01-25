f906768db21f6b1c96ff21768bafa3f1f50d195a
Maria Torralba <maria.torralba@king.com>
2025-01-25 21:18:17 +0100

    Grid working Step 1, Drag & Drop working

---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        GameObject dropped = eventData.pointerDrag;
+        if (dropped != null)
+        {
+            DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+            if (draggableItem != null)
+            {
+                draggableItem.parentAfterDrag = transform;
+            }
+        }
+
+        if (eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+}
