0c361414358e820f732008d322cdbb6192cff351
s-torralba <125572446+s-torralba@users.noreply.github.com>
2025-01-25 13:34:07 +0100

    Bubble moving up and sinus on x

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    public Vector2 speed;
+    public float frequency = 1f;
+
+    private Rigidbody2D _rb;
+    private float _time;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _time += Time.fixedDeltaTime;
+
+        float sinSpeedx = Mathf.Sin(_time * frequency) * speed.x;
+
+        _rb.velocity = new Vector2(sinSpeedx, speed.y);
+    }
+}
