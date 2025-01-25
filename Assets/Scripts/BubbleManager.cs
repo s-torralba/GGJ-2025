e5837daf8e119ab0a8b2348e3ba51979c94c2210
s-torralba <125572446+s-torralba@users.noreply.github.com>
2025-01-25 20:21:02 +0100

    Letters that will appear now generate from words and random letters

--- Assets/Scripts/BubbleManager.cs
+++ Assets/Scripts/BubbleManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 
@@ -13,12 +15,16 @@ public class BubbleManager : MonoBehaviour
 
     private float _timer;
     private string letterPath;
+    [SerializeField] private List<char> lettersToGenerate = new List<char>();
+    public List<string> wordsToGenerate = new List<string>();
+    public int randomLetter;
     [SerializeField] private LetterCollector letterCollector;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        List<string> list = new List<string>();
+        ChooseLetters(list, randomLetter);
     }
 
     // Update is called once per frame
@@ -32,12 +38,75 @@ public class BubbleManager : MonoBehaviour
         }
     }
 
+    public void ChooseLetters(List<string> words, int randomLettersAdded)
+    {
+        words = wordsToGenerate;
+        // Dictionary to track max occurrences of each letter across all words
+        Dictionary<char, int> resultLetterMap = new Dictionary<char, int>();
+        foreach (string word in words)
+        {
+            // Temporary dictionary for counting letters in the current word
+            Dictionary<char, int> currentWordMap = new Dictionary<char, int>();
+            string uppercaseWord = word.ToUpper();
+
+            foreach (char c in uppercaseWord)
+            {
+                if (currentWordMap.ContainsKey(c))
+                {
+                    currentWordMap[c]++;
+                }
+                else
+                {
+                    currentWordMap[c] = 1;
+                }
+            }
+
+            // Update the global letterCounts with the maximum occurrences from the current word
+            foreach (KeyValuePair<char, int> letterOnCurrentWord in currentWordMap)
+            {
+                if (resultLetterMap.ContainsKey(letterOnCurrentWord.Key))
+                {
+                    resultLetterMap[letterOnCurrentWord.Key] = Mathf.Max(resultLetterMap[letterOnCurrentWord.Key], letterOnCurrentWord.Value);
+                }
+                else
+                {
+                    resultLetterMap[letterOnCurrentWord.Key] = letterOnCurrentWord.Value;
+                }
+            }
+        }
+
+        // Create the final list of letters based on max counts
+        List<char> result = new List<char>();
+        foreach (KeyValuePair<char, int> kvp in resultLetterMap)
+        {
+            for (int i = 0; i < kvp.Value; i++)
+            {
+                result.Add(kvp.Key);
+            }
+        }
+
+        // Add random letters
+        for (int i = 0; i < randomLettersAdded; i++)
+        {
+            char randomLetter = (char)Random.Range('A', 'Z' + 1);
+            result.Add(randomLetter);
+        }
+
+        // Shuffle the result
+        result = result.OrderBy(_ => Random.value).ToList();
+
+        lettersToGenerate = result;
+    }
+
     void SpawnBubble()
     {
         // Spawn at random position within the area
         float x = Random.Range(-spawnArea.x / 2, spawnArea.x / 2);
         float y = Random.Range(-spawnArea.y / 2, spawnArea.y / 2);
         Vector3 spawnPosition = new Vector3(x + transform.position.x, y + transform.position.y, 0f);
+
+        char letter = lettersToGenerate.Last();
+        lettersToGenerate.Remove(letter);
 
         // Instantiate the bubble prefab
         GameObject bubble = Instantiate(bubblePrefab, spawnPosition, Quaternion.identity, transform);
@@ -49,7 +118,7 @@ public class BubbleManager : MonoBehaviour
         Vector3 randomScaleVector = new Vector3(randomScale, randomScale, randomScale);
         bubble.transform.localScale = randomScaleVector;
 
-        bubble.GetComponent<Bubble>().ChangeLetter("B");
+        bubble.GetComponent<Bubble>().ChangeLetter(letter.ToString());
         bubble.GetComponent<Bubble>().AssignCollector(letterCollector);
     }
 
